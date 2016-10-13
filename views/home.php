c5cd19def4865ec55a3f7f0b5d5c10cccaaab6a0
Jiedara <m-vi@bulko.net>
2016-10-13 18:34:08 +0200

    add home default template

---
+++ views/home.php
@@ -0,0 +1,14 @@
+<?php
+include('includes/header.php');
+?>
+
+<div class="container">
+	<div class="jumbotron">
+		<h1>Welcome in Weborama</h1>
+		<p class="lead">Edit this template in the /views/ folder and add some other routes to begin your fabulous adventure !</p>
+	</div>
+
+</div> <!-- /container -->
+
+<?php
+include('includes/footer.php');
