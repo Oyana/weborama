f7c34a9616b5dbde7149f79ec421b78a05f15235
Jiedara <m-vi@bulko.net>
2016-10-13 17:35:47 +0200

    create autoloader for framework

--- index.php
+++ index.php
@@ -1,1 +1,20 @@
 <?php
+
+include('config.php');
+require_once('routes.php');
+
+/*
+|-------------------------------------------------------
+| AUTOLOADER
+|-------------------------------------------------------
+|
+| Autoload Framework function in /framework/ directory
+| depending on some configuration (DEBUG_LVL, DB_TYPE, etc).
+| Note that only the index.php file of each folder is loaded.
+|
+*/
+
+$files = glob(__DIR__ . '/framework/*/index.php');
+foreach($files as $file){
+	include($file);
+}
