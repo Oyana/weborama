4ced0a089f78d6e925bd4580a6b9bf5ad44f5168
Jiedara <m-vi@bulko.net>
2016-10-13 16:05:10 +0200

    basic controllers

---
+++ controllers/Controller.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Controller
+ *
+ * @package 7agagner
+ * @subpackage controllers
+ */
+
+class Controller
+{
+	public function __construct(){
+
+	}
+}
---
+++ controllers/PageController.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * PageController
+ *
+ * @package 7agagner
+ * @subpackage controllers
+ */
+
+use Controller;
+
+class PageController extends Controller
+{
+
+}
