3e1c80ba77260bcc5cd9639d39505f2547e668f2
Jiedara <m-vi@bulko.net>
2016-10-13 17:35:05 +0200

    add config & htaccess

---
+++ config.php
@@ -0,0 +1,49 @@
+<?php
+
+/*
+|-------------------------------------------------------
+| DATABASE CONFIG
+|-------------------------------------------------------
+|
+| Database information for connection, like hostname
+| database name, user log and other.
+|
+*/
+
+/*- Type of the database (mysql, sqlite, ...) -*/
+define('DB_TYPE','mysql');
+
+/*- Host of the database (localhost, www.example.com, ...) -*/
+define('DB_HOST','localhost');
+
+/*- Name of the database -*/
+define('DB_NAME','bulko_7agagner_201610');
+
+/*- Prefix of the database tables -*/
+define('DB_PREFIX','');
+
+/*- Type of the database (root, admin, ...) -*/
+define('DB_USER','root');
+
+/*- Password of the database -*/
+define('DB_PASS','');
+
+
+/*
+|-------------------------------------------------------
+| WEBSITE CONFIG
+|-------------------------------------------------------
+|
+| website information for resources handling,
+| like base url, user log, debug level, etc
+|
+*/
+
+/*- Password of the database -*/
+define('SITE_URL','localhost/bulko_7agagner_201610');
+
+/*- choose beetween 3 debug lvl (0=none, 1=error viewing, 2=dev information) -*/
+define('DEBUG_LVL','2');
+
+/*- Is the website in maintenance ? (1/0) -*/
+define('MAINTENANCE','0');
