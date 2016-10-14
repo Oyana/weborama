86377fcbfd76fbe6a3ddc4267c32c7d719285cdf
Jiedara <m-vi@bulko.net>
2016-10-14 18:01:57 +0200

    add adminController

---
+++ controllers/AdminController.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * AdminController
+ *
+ * @package 7agagner
+ * @subpackage controllers
+ */
+
+class AdminController extends Controller
+{
+	public function __construct(){
+		parent::__construct();
+	}
+
+	public function home(){
+		if(!isset($_SESSION['user'])){
+			return redirect('auth/login');
+		}else {
+			return view('admin/home');
+		}
+	}
+}
