4d3400051a44ca1d2977d0d84a2a6be261d0e6c7
Jiedara <m-vi@bulko.net>
2016-10-14 11:24:42 +0200

    get debug display by DEBUG_LVL rank

--- framework/debugging/index.php
+++ framework/debugging/index.php
@@ -10,9 +10,33 @@
 |
 */
 
+
+error_reporting(0);
+switch(DEBUG_LVL){
+	case 0:
+		error_reporting(0);
+	break;
+	case 1:
+		error_reporting(E_ALL);
+		ini_set('display_errors',true);
+	break;
+	case 2:
+		error_reporting(E_ALL);
+		ini_set('display_errors',true);
+	break;
+	default:
+		error_reporting(E_ALL);
+		ini_set('display_errors',true);
+	break;
+}
+
 function debug($message){
-	echo '<pre>';
-	print_r($message);
-	echo '</pre>';
-	die();
+	if(DEBUG_LVL > 0){
+		echo '<pre>';
+		print_r($message);
+		echo '</pre>';
+		die();
+	}else {
+		return true;
+	}
 }
