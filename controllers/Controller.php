e248e412582968f380451118372cbbe40ca1dac1
Jiedara <m-vi@bulko.net>
2016-11-03 17:53:14 +0100

    fix message var

--- controllers/Controller.php
+++ controllers/Controller.php
@@ -12,24 +12,23 @@ class Controller
 	private $msgInfo = [];
 	private $msgWarning = [];
 	private $msgError = [];
-	public $handleRouting = true;
 
 	public function __construct(){
 		//Set Flash Session
 		if(!empty($_SESSION['msgSuccess'])){
-			$this->$msgSuccess = $_SESSION['msgSuccess'];
+			$this->msgSuccess = $_SESSION['msgSuccess'];
 			unset($_SESSION['msgSuccess']);
 		}
 		if(!empty($_SESSION['msgInfo'])){
-			$this->$msgInfo = $_SESSION['msgInfo'];
+			$this->msgInfo = $_SESSION['msgInfo'];
 			unset($_SESSION['msgInfo']);
 		}
 		if(!empty($_SESSION['msgWarning'])){
-			$this->$msgWarning = $_SESSION['msgWarning'];
+			$this->msgWarning = $_SESSION['msgWarning'];
 			unset($_SESSION['msgWarning']);
 		}
 		if(!empty($_SESSION['msgError'])){
-			$this->$msgError = $_SESSION['msgError'];
+			$this->msgError = $_SESSION['msgError'];
 			unset($_SESSION['msgError']);
 		}
 		return true;
