95f44c8fd63fea7674bb9065ecd3b48a2f630e2f
Jiedara <m-vi@bulko.net>
2016-11-03 18:06:22 +0100

    add config var

--- config.php
+++ config.php
@@ -22,7 +22,7 @@ define('DB_NAME','weborama');
 /*- Prefix of the database tables -*/
 define('DB_PREFIX','');
 
-/*- Type of the database (root, admin, ...) -*/
+/*- User of the database (root, admin, ...) -*/
 define('DB_USER','root');
 
 /*- Password of the database -*/
@@ -39,11 +39,16 @@ define('DB_PASS','');
 |
 */
 
-/*- Password of the database -*/
-define('SITE_URL','localhost/weborama');
+/*- Website Path (url & root) -*/
+define('SITE_URL','http://localhost/weborama');
+define('ROOT_PATH', dirname(__FILE__) );
 
 /*- choose beetween 3 debug lvl (0=none, 1=error viewing, 2=dev information) -*/
 define('DEBUG_LVL','2');
+
+/*- Localization of the website, according to the iso639-2 codes ( http://www.loc.gov/standards/iso639-2/php/code_list.php ) and formated like <lang>_<territory> -*/
+define('LANG','fr_FR');
+setlocale(LC_ALL, 'fr','fr_FR','fr_FR@euro','fr_FR.utf8','fr-FR','fra');
 
 /*- Is the website in maintenance ? (1/0) -*/
 define('MAINTENANCE','0');
