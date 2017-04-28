6a91b503ff121d0e23f62a4caba895e26f2aaf77
Martin Villanove <mvillanove@adneom.com>
2017-04-28 17:49:37 +0200

    change getter/setter for message controller

--- controllers/Controller.php
+++ controllers/Controller.php
@@ -8,65 +8,69 @@
 
 class Controller
 {
-	private $msgSuccess = [];
-	private $msgInfo = [];
-	private $msgWarning = [];
-	private $msgError = [];
+    private $msgSuccess = [];
+    private $msgInfo = [];
+    private $msgWarning = [];
+    private $msgError = [];
 
-	public function __construct(){
-		//Set Flash Session
-		if(!empty($_SESSION['msgSuccess'])){
-			$this->msgSuccess = $_SESSION['msgSuccess'];
-			unset($_SESSION['msgSuccess']);
-		}
-		if(!empty($_SESSION['msgInfo'])){
-			$this->msgInfo = $_SESSION['msgInfo'];
-			unset($_SESSION['msgInfo']);
-		}
-		if(!empty($_SESSION['msgWarning'])){
-			$this->msgWarning = $_SESSION['msgWarning'];
-			unset($_SESSION['msgWarning']);
-		}
-		if(!empty($_SESSION['msgError'])){
-			$this->msgError = $_SESSION['msgError'];
-			unset($_SESSION['msgError']);
-		}
-		return true;
-	}
+    public function __construct(){
+        //Set Flash Session
+        if(!empty($_SESSION['msgSuccess'])){
+            $this->msgSuccess = $_SESSION['msgSuccess'];
+            unset($_SESSION['msgSuccess']);
+        }
+        if(!empty($_SESSION['msgInfo'])){
+            $this->msgInfo = $_SESSION['msgInfo'];
+            unset($_SESSION['msgInfo']);
+        }
+        if(!empty($_SESSION['msgWarning'])){
+            $this->msgWarning = $_SESSION['msgWarning'];
+            unset($_SESSION['msgWarning']);
+        }
+        if(!empty($_SESSION['msgError'])){
+            $this->msgError = $_SESSION['msgError'];
+            unset($_SESSION['msgError']);
+        }
+        return true;
+    }
 
-	public function setSuccess($message){
-		$this->msgSuccess[] = $message;
-		$_SESSION['msgSuccess'] = $this->msgSuccess;
-		return true;
-	}
-	public function getSuccess(){
-		return $this->msgSuccess;
-	}
+    public function successMessage($message = null){
+        if($message){
+              $this->msgSuccess[] = $message;
+              $_SESSION['msgSuccess'] = $this->msgSuccess;
+              return true;
+        }else {
+            return $this->msgSuccess;
+        }
+    }
 
-	public function setInfo($message){
-		$this->msgInfo[] = $message;
-		$_SESSION['msgInfo'] = $this->msgInfo;
-		return true;
-	}
-	public function getInfo(){
-		return $this->msgInfo;
-	}
+    public function infoMessage($message = null){
+        if($message){
+              $this->msgInfo[] = $message;
+              $_SESSION['msgInfo'] = $this->msgInfo;
+              return true;
+        }else {
+            return $this->msgInfo;
+        }
+    }
 
-	public function setWarning($message){
-		$this->msgWarning[] = $message;
-		$_SESSION['msgWarning'] = $this->msgWarning;
-		return true;
-	}
-	public function getWarning(){
-		return $this->msgWarning;
-	}
+    public function warningMessage($message = null){
+        if($message){
+              $this->msgWarning[] = $message;
+              $_SESSION['msgWarning'] = $this->msgWarning;
+              return true;
+        }else {
+            return $this->msgWarning;
+        }
+    }
 
-	public function setError($message){
-		$this->msgError[] = $message;
-		$_SESSION['msgError'] = $this->msgError;
-		return true;
-	}
-	public function getError(){
-		return $this->msgError;
-	}
+    public function errorMessage($message = null){
+        if($message){
+              $this->msgError[] = $message;
+              $_SESSION['msgError'] = $this->msgError;
+              return true;
+        }else {
+            return $this->msgError;
+        }
+    }
 }
